56fb99c58a6dad247f113289e6bcba9070736862
Nora Abbassi-Mohadjelgit config --global user.name Nora <noraabbassi-mohadjel@d207-023-174-189.wireless.sfu.ca>
2019-10-14 16:45:27 -0700

    ha

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ app.use(express.urlencoded({ extended:false }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-app.get('/tokimon', (req, res) => {
-    res.render('tokimon');
-   });
-
-   app.get('/AddTokimon', (req, res) => {
-    res.render('AddTokimon');
-   });
-
 app.get('/', (req, res) => {
     var tableCreationQuery = `CREATE TABLE IF NOT EXISTS public.tokimons (tid serial, name varchar(20), height int, weight int, flying int, fighting int, fire int, water int, electric int, ice int, total int, trainername varchar(20))`
     pool.query(tableCreationQuery, (error, result) => {
