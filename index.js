3923fc6b9a7f0532e20148c5b0c86bfe3581a27f
Nora Abbassi-Mohadjelgit config --global user.name Nora <noraabbassi-mohadjel@d207-023-174-189.wireless.sfu.ca>
2019-10-15 19:49:50 -0700

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL;
+  connectionString: process.env.DATABASE_URL
   //connectionString: 'postgres://postgres:@localhost/toki_data'
 });
 pool.connect();
